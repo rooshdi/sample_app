6cd8590c291831d36ad673917b6d5aa84023bab6
Rooshdi Ali <rooshdiali@gmail.com>
2012-03-21 19:37:27 -0500

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.1'
+gem 'rails', '3.2.2'
+gem 'bootstrap-sass', '2.0.1'
 gem 'bcrypt-ruby', '3.0.1'
 gem 'pg', '0.12.2'
 
@@ -28,4 +29,5 @@ group :test do
   gem 'growl', '1.0.3'
   gem 'guard-spork', '0.5.2'
   gem 'spork', '0.9.0'
+  gem 'factory_girl_rails', '1.7.0'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+
+  root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
 
@@ -8,8 +10,6 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
 
   match '/contact', to: 'static_pages#contact'
-
-  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
